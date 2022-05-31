ea17280d87df07540e9053ccad3997bcffd88aa5
teodora <tbd@shippii.com>
2022-06-01 00:53:06 +0300

    removed json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,11 +1,16 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [feedback, setFeedback] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [feedback, setFeedback] = useState([
+    { id: "1", rating: 8, text: "Feedback 01" },
+    { id: "2", rating: 4, text: "Feedback 02" },
+    { id: "3", rating: 7, text: "Feedback 03" },
+  ]);
   const [popup, setPopup] = useState({
     show: false,
     id: null,
@@ -15,18 +20,18 @@ export const FeedbackProvider = ({ children }) => {
     edit: false,
   });
 
-  useEffect(() => {
-    fetchFeedback();
-  }, []);
+  // useEffect(() => {
+  //   fetchFeedback();
+  // }, []);
 
   const fetchFeedback = () => {
-    axios
-      .get("/feedback")
-      .then(({ data }) => {
-        setFeedback(data);
-        setIsLoading(false);
-      })
-      .catch((error) => console.log(error));
+    // axios
+    //   .get("/feedback")
+    //   .then(({ data }) => {
+    //     setFeedback(data);
+    //     setIsLoading(false);
+    //   })
+    //   .catch((error) => console.log(error));
   };
 
   const deleteFeedback = (id) => {
@@ -36,18 +41,25 @@ export const FeedbackProvider = ({ children }) => {
     });
   };
   const handleDeleteTrue = () => {
+
     if (popup.show && popup.id) {
-      axios
-        .delete(`/feedback/${popup.id}`)
-        .then((res) => {
-          setPopup({
+      setFeedback(feedback.filter((item) => item.id !== popup.id));
+      // axios
+      //   .delete(`/feedback/${popup.id}`)
+      //   .then((res) => {
+      //     setPopup({
+      //       show: false,
+      //       id: null,
+      //     });
+       
+          
+      //   })
+      //   .catch((e) => console.log(e));
+
+            setPopup({
             show: false,
             id: null,
           });
-          fetchFeedback()
-          
-        })
-        .catch((e) => console.log(e));
     }
   };
 
@@ -59,14 +71,16 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   const addFeedback = (newFeedback) => {
-    axios
-      .post("/feedback", newFeedback)
-      .then(({data}) => {
-         setFeedback([data,...feedback])
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  //   axios
+  //     .post("/feedback", newFeedback)
+  //     .then(({data}) => {
+  //       //  setFeedback([data,...feedback])
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error);
+  //     });
   };
 
   const editFeedback = (item) => {
@@ -76,12 +90,15 @@ export const FeedbackProvider = ({ children }) => {
     });
   };
   const updateFeedback = (id, upItem) => {
-    axios
-    .put(`/feedback/${id}`,upItem).then(({data})=>{
-      setFeedback(
-        feedback.map((item) => (item.id === id ? data : item))
-      );
-    }).catch((e)=>console.log(e))
+    setFeedback(
+      feedback.map((item) => (item.id === id ? { ...item, ...upItem } : item))
+    );
+    // axios
+    // .put(`/feedback/${id}`,upItem).then(({data})=>{
+    //   setFeedback(
+    //     feedback.map((item) => (item.id === id ? data : item))
+    //   );
+    // }).catch((e)=>console.log(e))
   
   };
 
